922ced122b54554dfd79350806b2c434cad485f2
John Wood <johnnathanwood@gmail.com>
2018-10-18 16:00:39 -0500

    added api

--- src/scripts/eventListener.js
+++
@@ -1,3 +0,0 @@
-const button = document.querySelector("#populateDataBase")
-
-.addEventListener("click", populateDB)
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,15 +1,9 @@
-const createJournalEntry = () => {
-    const entries = {
-        entry:[],
+const journalEntries = [
+    {
+        date: "07/24/2018",
+        concept: "Array methods",
+        entry: "We learned about 4 different array methods today. forEach made sense, but the others still confuse me.",
+        mood: "Ok"
     }
-
-    return entries
-
-}
-const entries = []
-const stringified = JSON.stringify(entries)
-
-localStorage.setItem("entries",stringified)
-
-
+]
 
